4a4a4a5a7ed4352ec7b186a87dc8e3cd29a7aeae
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-03-09 13:29:12 +0700

    using navigation graph

--- app/src/main/java/com/example/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivity.kt
@@ -1,73 +1,13 @@
 package com.example.pokedex.ui
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.R
-import com.example.pokedex.adapter.GridPokemonAdapter
-import com.example.pokedex.adapter.ListPokemonAdapter
-import com.example.pokedex.model.Pokemon
-import com.example.pokedex.util.PokemonData
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var rvPokemon: RecyclerView
-//    private var list: ArrayList<Pokemon> = arrayListOf()
-//    private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        rvPokemon = findViewById(R.id.rv_pokemon)
-//        rvPokemon.setHasFixedSize(true)
-//
-//        list.addAll(PokemonData.listData)
-//        chooseLayout()
     }
-//
-//    private fun chooseLayout() {
-//        if (isLinearLayoutManager) {
-//            rvPokemon.layoutManager = LinearLayoutManager(this)
-//            val listPokemonAdapter = ListPokemonAdapter(list)
-//            rvPokemon.adapter = listPokemonAdapter
-//        } else {
-//            rvPokemon.layoutManager = GridLayoutManager(this, 2)
-//            val gridHeroAdapter = GridPokemonAdapter(list)
-//            rvPokemon.adapter = gridHeroAdapter
-//        }
-//    }
-//
-//    private fun setIcon(menuItem: MenuItem?) {
-//        if (menuItem == null)
-//            return
-//        menuItem.icon =
-//            if (isLinearLayoutManager)
-//                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
-//            else ContextCompat.getDrawable(this, R.drawable.ic_list_layout)
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        val layoutButton = menu?.findItem(R.id.action_switch_layout)
-//        setIcon(layoutButton)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            R.id.action_switch_layout -> {
-//                isLinearLayoutManager = !isLinearLayoutManager
-//                chooseLayout()
-//                setIcon(item)
-//
-//                return true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
 }
